01850a99b36b16b3f7423a38460bbd7a7980c1f7
a-rahman092 <72246399+a-rahman092@users.noreply.github.com>
2021-02-20 23:01:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header ">
-          <img src={logo} alt="logo" className="App-logo" />
-          <h1 className="App-title">Welcome to Your MDB React App</h1>
+          
+          <h1 className="App-title">Welcome to My React App</h1>
         </header>
-        <p className="mb-2">The application is configured and ready to import our components.</p>
-        <Button href="https://mdbootstrap.com/react/" target="blank" color="light-blue"><strong>Check out our docs!</strong></Button>
+        <p>Mohd Abdul Rahman</p>
       </div>
     );
   }
